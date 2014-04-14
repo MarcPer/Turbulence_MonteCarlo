7275363c13c5100821b16ead053d7cfde86ccf8a
MarcPer <m_tonho@yahoo.com.br>
2014-04-14 14:26:39 -0300

    Removes unnecessary normalization

--- TurbulenceSimulator.m
+++ TurbulenceSimulator.m
@@ -510,7 +510,7 @@ classdef TurbulenceSimulator<handle
                     obj.phaseScreenProfiles = generateScreen(obj.simulationParameters);
                     outputField =  ...
                         obj.getFieldForEachTransverseSeparation();
-                    IavgRe = IavgRe + Util.normalize(abs(outputField).^2);
+                    IavgRe = IavgRe + abs(outputField).^2;
                     UserInput.updateWaitBar(obj.abortButtonHandle, iRe, nRe);
                 end
                 delete(obj.abortButtonHandle);
